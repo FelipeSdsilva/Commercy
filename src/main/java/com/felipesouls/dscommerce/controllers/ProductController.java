fca4a675dae297ee7c3fd37a2515e94509524ac6
Felipe Sousa <felipe.fps09@hotmail.com>
2023-12-09 10:12:51 -0300

    Implemented CRUD for class Product.

--- src/main/java/com/felipesouls/dscommerce/controllers/ProductController.java
+++ src/main/java/com/felipesouls/dscommerce/controllers/ProductController.java
@@ -1,9 +1,13 @@
 package com.felipesouls.dscommerce.controllers;
 
+import com.felipesouls.dscommerce.dto.ProductDTO;
 import com.felipesouls.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -12,5 +16,31 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> getProductsPagined(Pageable pageable) {
+        return ResponseEntity.ok(productService.allProductsPaginated(pageable));
+    }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> getProductPerId(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.findProductPerId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> postProduct(@RequestBody ProductDTO productDTO) {
+        productDTO = productService.insertNewProduct(productDTO);
+        var uri = fromCurrentRequest().path("/{id}").buildAndExpand(productDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(productDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> putProduct(@PathVariable Long id,@RequestBody ProductDTO productDTO) {
+        return ResponseEntity.ok(productService.updateProductPerId(id, productDTO));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delrProductPerId(@PathVariable Long id) {
+        productService.deleteProductPerId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/felipesouls/dscommerce/services/ProductService.java
+++ src/main/java/com/felipesouls/dscommerce/services/ProductService.java
@@ -1,7 +1,66 @@
 package com.felipesouls.dscommerce.services;
 
+import com.felipesouls.dscommerce.dto.ProductDTO;
+import com.felipesouls.dscommerce.entities.Product;
+import com.felipesouls.dscommerce.repositories.ProductRepository;
+import com.felipesouls.dscommerce.services.exceptions.DatabaseException;
+import com.felipesouls.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> allProductsPaginated(Pageable pageable) {
+        return productRepository.findAll(pageable).map(ProductDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findProductPerId(Long id) {
+        return new ProductDTO(productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id + " Id not found")));
+    }
+
+    @Transactional
+    public ProductDTO insertNewProduct(ProductDTO productDTO) {
+        Product product = productRepository.save(new Product(productDTO));
+        return new ProductDTO(product);
+    }
+
+    @Transactional
+    public ProductDTO updateProductPerId(Long id, ProductDTO productDTO) {
+        try {
+            Product product = productRepository.getReferenceById(id);
+            convertDtoInEntity(productDTO,product);
+            productRepository.save(product);
+            return new ProductDTO(product);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id + " Id not found");
+        }
+    }
+
+    public void deleteProductPerId(Long id) {
+        try {
+            productRepository.deleteById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("");
+        }
+    }
+
+    private void convertDtoInEntity(ProductDTO productDTO, Product product){
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setImgUrl(productDTO.getImgUrl());
+    }
 }
