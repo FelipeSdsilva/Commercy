89ac14a11e149b58eea32a3ff627e455227d7a29
Felipe Sousa <felipe.fps09@hotmail.com>
2023-12-10 11:01:28 -0300

    Implements routs for CRUD in CategoryController.

--- src/main/java/com/felipesouls/dscommerce/controllers/CategoryController.java
+++ src/main/java/com/felipesouls/dscommerce/controllers/CategoryController.java
@@ -1,9 +1,14 @@
 package com.felipesouls.dscommerce.controllers;
 
+import com.felipesouls.dscommerce.dto.CategoryDTO;
 import com.felipesouls.dscommerce.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -11,4 +16,32 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryDTO>> getCategoryPerPaged(Pageable pageable) {
+        return ResponseEntity.ok(categoryService.allcategoriesPaginated(pageable));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryPerId(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.findCategoryPerId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> postNewCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO categoryDTO1 = categoryService.insertNewCategory(categoryDTO);
+        var uri = fromCurrentRequest().path("/{id}").buildAndExpand(categoryDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(categoryDTO1);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> putCategoryPerId(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
+        return ResponseEntity.ok(categoryService.updateCategoryPerId(id, categoryDTO));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteCategoryPerId(@PathVariable Long id) {
+        categoryService.deleteCategoryPerId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
