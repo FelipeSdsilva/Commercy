3251aefefac2da362246bacd3642cac01a619504
Felipe Sousa <felipe.fps09@hotmail.com>
2024-01-13 07:43:08 -0300

    Initialized service layer for OrderService.

--- src/main/java/com/felipesouls/dscommerce/services/OrderService.java
+++ src/main/java/com/felipesouls/dscommerce/services/OrderService.java
@@ -1,7 +1,75 @@
 package com.felipesouls.dscommerce.services;
 
+import com.felipesouls.dscommerce.dto.OrderDTO;
+import com.felipesouls.dscommerce.dto.ProductDTO;
+import com.felipesouls.dscommerce.entities.Order;
+import com.felipesouls.dscommerce.entities.OrderItem;
+import com.felipesouls.dscommerce.repositories.OrderItemRepository;
+import com.felipesouls.dscommerce.repositories.OrderRepository;
+import com.felipesouls.dscommerce.repositories.ProductRepository;
+import com.felipesouls.dscommerce.services.exceptions.DatabaseException;
+import com.felipesouls.dscommerce.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public Page<OrderDTO> paginatedAllOrders(Pageable pageable) {
+        return orderRepository.findAll(pageable).map(order -> new OrderDTO(order, order.getItems()));
+    }
+
+    @Transactional(readOnly = true)
+    public OrderDTO findOrderPerId(Long id) {
+        return new OrderDTO(orderRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id + "Not found")));
+    }
+
+    @SuppressWarnings("unused")
+    @Transactional
+    public OrderDTO createAnewOrder(OrderDTO orderDTO) {
+        return null;
+    }
+
+    @Transactional
+    public OrderDTO updateOrderPerId(@SuppressWarnings("unused") Long id, @SuppressWarnings("unused") OrderDTO orderDTO) {
+        return null;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void deleteOrderPerId(Long id) {
+        if (orderRepository.existsById(id))
+            throw new ResourceNotFoundException(id + " id not found!");
+        try {
+            orderRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrate violation!");
+        }
+    }
+
+    private void converterDtoForEntity(Order order, OrderDTO orderDTO) {
+        order.getItems().clear();
+
+        orderDTO.getItems().forEach(orderItemDTO -> {
+            orderItemDTO.setProductDTO(new ProductDTO(productRepository.getReferenceById(orderItemDTO.getProduct().getId())));
+            orderItemDTO.setOrder(orderDTO);
+            orderItemRepository.save(new OrderItem(orderItemDTO));
+        });
+
+    }
+
 }
