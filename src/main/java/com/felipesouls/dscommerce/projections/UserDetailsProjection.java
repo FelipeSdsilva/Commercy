b01180f1c027674074ec9a25b4950f736a0e412b
Felipe Sousa <felipe.fps09@hotmail.com>
2024-01-28 08:24:08 -0300

    New interface projection for return Authority per email.

---
+++ src/main/java/com/felipesouls/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,10 @@
+package com.felipesouls.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+
+}
