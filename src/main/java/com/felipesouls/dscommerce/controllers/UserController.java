e5cf92d1ef6ac844f841e9edeb45881670127194
Felipe Sousa <felipe.fps09@hotmail.com>
2024-01-28 08:33:11 -0300

    CRUD for user.

--- src/main/java/com/felipesouls/dscommerce/controllers/UserController.java
+++ src/main/java/com/felipesouls/dscommerce/controllers/UserController.java
@@ -1,9 +1,19 @@
 package com.felipesouls.dscommerce.controllers;
 
+import com.felipesouls.dscommerce.dto.UserDTO;
+import com.felipesouls.dscommerce.dto.UserInsertDTO;
+import com.felipesouls.dscommerce.dto.UserUpdateDTO;
 import com.felipesouls.dscommerce.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -12,5 +22,35 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> getAllUserPaginated(Pageable pageable) {
+        return ResponseEntity.ok().body(userService.listUserPagined(pageable));
+    }
 
+    @GetMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_OPERATOR')")
+    public ResponseEntity<UserDTO> getUserPerId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.retriveUserPerId(id));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_OPERATOR')")
+    public ResponseEntity<UserDTO> postNewUser(@Valid @RequestBody UserInsertDTO dto) {
+        UserDTO insertDTO = userService.insertNewUser(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(insertDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(insertDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_OPERATOR')")
+    public ResponseEntity<UserDTO> putUser(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+        return ResponseEntity.ok().body(userService.updateUserPerId(id, dto));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Void> deleteUserPerId(@PathVariable Long id) {
+        userService.deleteUserPerId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/felipesouls/dscommerce/services/UserService.java
+++ src/main/java/com/felipesouls/dscommerce/services/UserService.java
@@ -1,7 +1,111 @@
 package com.felipesouls.dscommerce.services;
 
+import com.felipesouls.dscommerce.dto.RoleDTO;
+import com.felipesouls.dscommerce.dto.UserDTO;
+import com.felipesouls.dscommerce.dto.UserInsertDTO;
+import com.felipesouls.dscommerce.dto.UserUpdateDTO;
+import com.felipesouls.dscommerce.entities.Role;
+import com.felipesouls.dscommerce.entities.User;
+import com.felipesouls.dscommerce.projections.UserDetailsProjection;
+import com.felipesouls.dscommerce.repositories.RoleRepository;
+import com.felipesouls.dscommerce.repositories.UserRepository;
+import com.felipesouls.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<UserDTO> listUserPagined(Pageable pageable) {
+        return userRepository.findAll(pageable).map(UserDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO retriveUserPerId(Long id) {
+        return new UserDTO(userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Id not found " + id)));
+    }
+
+    @Transactional
+    public UserDTO insertNewUser(UserInsertDTO dto) {
+        User user = new User();
+        converterDtoForEntity(user, dto);
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user = userRepository.save(user);
+        return new UserDTO(user);
+    }
+
+    @Transactional
+    public UserDTO updateUserPerId(Long id, UserUpdateDTO dto) {
+        try {
+            User user = userRepository.getReferenceById(id);
+            converterDtoForEntity(user, dto);
+            user.setPassword(passwordEncoder.encode(dto.getPassword()));
+            user = userRepository.save(user);
+            return new UserDTO(user);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void deleteUserPerId(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        } catch (EntityNotFoundException e) {
+
+        }
+    }
+
+    private void converterDtoForEntity(User user, UserDTO dto) {
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPhone(dto.getPhone());
+
+        user.getRoles().clear();
+
+        for (RoleDTO roleDTO : dto.getRoles()) {
+            Role role = roleRepository.getReferenceById(roleDTO.getId());
+            user.getRoles().add(role);
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
 }
