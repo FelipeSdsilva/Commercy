6b6872998aee162f738e20b85c96a9151ea07598
Felipe Sousa <felipe.dsilva@terceiroslinx.com.br>
2024-03-29 19:31:51 -0300

    Created methods for resolving problem N+1 consult and search for name.

--- src/main/java/com/felipesouls/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/felipesouls/dscommerce/repositories/ProductRepository.java
@@ -1,7 +1,18 @@
 package com.felipesouls.dscommerce.repositories;
 
 import com.felipesouls.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(name = "findAllProductsWithCategories")
+    Page<Product> findAllProductsWithCategories(Pageable pageable);
+
+    @Query(name = "paginationAllProductsAndSearchPerName")
+    Page<Product> paginationAllProductsAndSearchPerName(String name, Pageable pageable);
 }
