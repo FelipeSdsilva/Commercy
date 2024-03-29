c0c3c66b62744b404d43f62463a85e1d70f87d5b
Felipe Sousa <felipe.dsilva@terceiroslinx.com.br>
2024-03-29 19:29:32 -0300

    Altering return for method findByEmail for return a optional type.

--- src/main/java/com/felipesouls/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/felipesouls/dscommerce/repositories/UserRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     @Query(name = "searchUserAndRolesByEmail")
     List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
 }
